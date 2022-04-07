e19a9ce4ece60c72e7c382d99b4c2c6e90ea22ab
Mczionjohnson@gmail.com <mczionjohnson@gmail.com>
2022-04-07 15:26:33 -0700

    using puts func

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
- *main - prints the "Programming is like building a multilingual puzzle
- *return - reurns zero if successful, non zero otherwise
+ * main - prints the "Programming is like building a multilingual puzzle
+ * return: returns zero if successful, non zero otherwise
  */
 int main(void)
 {
-
   puts("\"Programming is like building a multilingual puzzle");
   return (0);
 }
