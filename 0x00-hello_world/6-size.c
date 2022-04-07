507cff67c9ffd0d41f23d2c7ff5a9b1fcbbd1edc
Mczionjohnson@gmail.com <mczionjohnson@gmail.com>
2022-04-07 16:53:44 -0700

    list of types in sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - calls the printf func
+ * Return: zero for success, non zero for otherwise
+ */
+int main(void)
+{
+	printf("size of char: %d bytes(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));			printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
+}
