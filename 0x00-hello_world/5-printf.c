1d4342bb4dc0699534177f5ec91d2ef24d73d119
Mczionjohnson@gmail.com <mczionjohnson@gmail.com>
2022-04-07 16:04:19 -0700

    use printf func

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - calls the prinf function
+ * return: zero if successful, non zero otherwise 
+ * */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n)";
+	return (0);
+}
