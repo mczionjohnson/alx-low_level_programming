eab1f115e100910e8990398fcac60c3d3c871074
Mczionjohnson@gmail.com <mczionjohnson@gmail.com>
2022-04-12 06:58:08 -0700

    print letters 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints letters 10x
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int line = 0
+
+
+	while (line < 10)
+	{
+
+		char ch = 'a';
+
+		for (ch <= 'z'; ch++)
+		{
+			_putchar('ch');
+
+		}
+		_putchar('\n');
+		line++;
+	}
+
+
+}
