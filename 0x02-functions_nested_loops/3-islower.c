e55b6b379153fa1303909ff5dd353d4d39c978b2
Mczionjohnson@gmail.com <mczionjohnson@gmail.com>
2022-04-12 09:59:39 -0700

    finding lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * _islower - find lowercase
- *
+ * _islower - checks lowercase
+ * @c: the character to check
+ * Return: 1 if c is lowercase, 0 otherwise
  */
 
 int _islower(int c)
