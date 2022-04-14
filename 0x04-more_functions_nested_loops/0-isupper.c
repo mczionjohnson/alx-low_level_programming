e00f140ee52745f1141f2e584d687f306d9d4b94
Mczionjohnson@gmail.com <mczionjohnson@gmail.com>
2022-04-14 02:19:38 -0700

    checks for digits 0-9

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,7 +3,7 @@
 /**
  * _isupper - checks for uppercase
  * @c: the character to check
- * Returns 1 if successful and 0 otherwise
+ * Returns: 1 if successful and 0 otherwise
  */
 
 int _isupper(int c)
