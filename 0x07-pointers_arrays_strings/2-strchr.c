cc11da9310d5d2e31b038ac35878d964cfbf2a0f
Mczionjohnson@gmail.com <mczionjohnson@gmail.com>
2022-04-26 07:56:04 -0700

    function to search in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,12 +1,11 @@
 #include "main.h"
 /**
- * *_strchr - function that locates a character in a string.
+ * _strchr - function that locates a character in a string.
  *
  * @s: string to receive and return
  * @c: character to receive
  * Return: char
  */
-
 char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
